2ab07a58ea9a0f0544a6742d1e6003e8c9348830
Hossein Amin <hossein@aminbros.com>
2019-11-29 16:54:17 +0300

    changes for package 0.0.3

--- setup.py
+++ setup.py
@@ -1,23 +1,26 @@
-from setuptools import setup
-
+from setuptools import setup, find_packages
 
 setup(
-    name="ebay-oauth-python-client",
-    version="0.0.1",
-    packages=['ebayoauthclient'],
-    license="LICENSE.txt",
+    name="ebayoauthclient",
+    version="0.0.3",
+    packages=find_packages(),
+    license="LICENSE",
     description="Python OAuth SDK: Get OAuth tokens for eBay public APIs",
     author="Hossein Amin",
+    author_email="hossein@aminbros.com",
     url="https://github.com/hosseinamin/ebay-oauth-python-client",
     classifiers=[
         'Operating System :: OS Independent',
         'Programming Language :: Python',
-        'Programming Language :: Python :: 2'
+        'Programming Language :: Python :: 2',
         'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: Apache Software License',
     ],
     install_requires=[
-        'selenium>=3.141.0,<4',
         'requests>=2.21.0,<3',
         'PyYAML>=5.1,<6',
+    ],
+    setup_requires=[
+        'selenium>=3.141.0,<4',
     ]
 )
